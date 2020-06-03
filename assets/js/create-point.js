5ffea64a1502d4dedcf7003c1b2a4b55c48554b8
Arthur Rodrigues <artgui3282@gmail.com>
2020-06-03 15:08:21 -0300

    Add files via upload
    JS Aula 3

--- assets/js/create-point.js
+++ assets/js/create-point.js
@@ -2,7 +2,7 @@ function script () {
    
 
     function populateState () {
-        const selectState = document.querySelector("select[name=companyState]");
+        const selectState = document.querySelector('select[name=companyState]');
 
         fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
         .then( (res) => { return res.json() })
@@ -14,32 +14,58 @@ function script () {
     }
 
     function getCities(event) {
-        const selectCity = document.querySelector("[name=companyCity]");
+        const selectCity = document.querySelector('[name=companyCity]');
         const stateValue = event.target.value;
-        const stateInput = document.querySelector("[name=state]");
+        const stateInput = document.querySelector('[name=state]');
+        
         const indexOfSelectedState = event.target.selectedIndex;
-
         stateInput.value = event.target.options[indexOfSelectedState].text;
 
         const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateValue}/municipios`;
-
+        
+        selectCity.innerHTML = '<option value> Selecione a Cidade </option>';
+        selectCity.disabled = true;
 
         fetch(url)
         .then( (res) => { return res.json() })
         .then( cities => {
             for( const city of cities){
-                selectCity.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+                selectCity.innerHTML += `<option value="${city.nome}"> ${city.nome} </option>`;
             }
-            selectCity.disabled = false;
+                selectCity.disabled = false;
         })
     }
 
     populateState();
 
     document
-        .querySelector("select[name=companyState]")
-        .addEventListener('change', getCities)
+        .querySelector('select[name=companyState]')
+        .addEventListener("change", getCities)
+
+
+    const itemsToCollect = document.querySelectorAll('.items-grid li');
+
+    for (const item of itemsToCollect) {
+        item.addEventListener('click', handleSelectedItem);
+    }
+
+    const collectedItems = document.querySelector('input[name=items]');
+
+    let selectedItems = [];
+
+    function handleSelectedItem (event) {
+        const item = event.target;
+        const itemId = item.dataset.id;
+        const alreadySelected = selectedItems.findIndex( (item) => {
+            return item === itemId
+        })
+
+        item.classList.toggle('selected');
+
+        alreadySelected !== -1 ?  selectedItems = selectedItems.filter( item => {return item !== itemId}) : selectedItems.  push(itemId);
+
+        collectedItems.value = selectedItems;
+
+    }
 }
-
-
 script();
---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+function script () {
+    const buttonSearch = document.querySelector('#page-home main a');
+    const modal = document.querySelector('#modal');
+    const closeSearch = document.querySelector('#modal .header a');
+
+    buttonSearch.addEventListener('click', () => {
+        modal.classList.remove('hide');
+    })
+
+    closeSearch.addEventListener('click', () => {
+        modal.classList.add('hide');
+    })
+}
+script();
